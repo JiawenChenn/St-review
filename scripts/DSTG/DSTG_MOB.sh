5b17ca249d89006b5892df7015b1e9993f9c730c
weifangliu <weifang.liu2012@gmail.com>
2022-05-19 21:24:14 -0500

    Add files via upload

---
+++ scripts/DSTG/DSTG_MOB.sh
@@ -0,0 +1,14 @@
+# seqFISH+ MOB internal default gene set
+
+module add python/3.7.9
+module add r/4.0.3
+module add gcc/4.9.1
+module add clang/6.0
+
+cd DSTG
+# converted ./processed_data/MOB/ref/internal/ob.sc_cnt.1950cell.tsv and ./processed_data/MOB/ref/internal/ob.sc.1950cell.mta.tsv to RDS
+Rscript convert_data.R ./processed_data/MOB/ref/internal/scRNA_seqfish_count.RDS ./processed_data/MOB/st_mob_seqfish_count.RDS ./processed_data/MOB/ref/internal/scRNA_seqfish_label.RDS
+python3 train.py 
+
+# results will be in DSTG_Result/predict_output.csv
+# cell type labels can be found in Infor_Data/ST_label/ST_label_1.csv
